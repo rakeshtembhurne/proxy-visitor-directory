d1ca9fa483f77b580feaece07dd677582044a64d
Rakesh Tembhurne <rakesh@tembhurne.com>
2019-10-30 09:47:23 +0530

    Updates

---
+++ precache-manifest.98b6f4f7cd83aac01a4fc0186622aea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34eed0ba0462689673c554c61622d2f",
+    "url": "/proxy-visitor-directory/index.html"
+  },
+  {
+    "revision": "0ad0af3cd720c1f272da",
+    "url": "/proxy-visitor-directory/static/css/main.e5c66f0c.chunk.css"
+  },
+  {
+    "revision": "dddac1fd006b212ed8fe",
+    "url": "/proxy-visitor-directory/static/js/2.5c296cf3.chunk.js"
+  },
+  {
+    "revision": "0ad0af3cd720c1f272da",
+    "url": "/proxy-visitor-directory/static/js/main.d49a66ee.chunk.js"
+  },
+  {
+    "revision": "8621032831542992db17",
+    "url": "/proxy-visitor-directory/static/js/runtime-main.ab65437f.js"
+  }
+]);
